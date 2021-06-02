79668126df313877ed4e197f86195f798556c014
qkrdud1123 <80142283+qkrdud1123@users.noreply.github.com>
2021-06-02 09:06:36 +0900

    main_210602
    main_210602

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -13,17 +13,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main extends Application {
-
+	
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/View/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+		 System.out.println(Thread.currentThread().getName() + " : start()");
+        Parent root = FXMLLoader.load(getClass().getResource("/View/sample.fxml"));//이름 넣기
+        primaryStage.setTitle("취업동향분석 앱 서비스");//타이틀
+        primaryStage.setScene(Scene);//씬
+        primaryStage.show();//윈도우
+    }
+    
+     @Override
+    public void stop() throws Exception{
+		 System.out.println(Thread.currentThread().getName() + " : stop()");
+    	super.stop();
     }
 
-
     public static void main(String[] args) throws IOException {
-
+        System.out.println(Thread.currentThread().getName() + " : main()");
+        launch(args);
     }
 }
