dedd582b784041e83810339fb50a934949e1d8bc
canoe <ehgus430880@gmail.com>
2021-06-08 21:58:44 +0900

    style update

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,7 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import com.jfoenix.controls.JFXDecorator;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -20,8 +22,12 @@ public class Main extends Application {
         JFXDecorator decorator = new JFXDecorator(primaryStage, root);
         decorator.setCustomMaximize(true);
         decorator.autosize();
+
         Scene scene = new Scene(decorator, 1280, 720);
+        scene.setFill(Color.TRANSPARENT);
         scene.getStylesheets().add(getClass().getResource("/View/CSS/jfx.css").toExternalForm());
+
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setTitle("취업동향분석 앱 서비스");//타이틀
         primaryStage.setScene(scene);
         primaryStage.show();//윈도우
