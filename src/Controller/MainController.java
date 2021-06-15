4992fb4757f6e568720e647377455abb27b60d73
canoe <ehgus430880@gmail.com>
2021-06-16 06:34:07 +0900

    style

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -30,16 +30,21 @@ public class MainController{
     private Chart barChartLayout =null;
     private ImageView mapLayout = null;
     private Chart lineChartLayout = null;
+    private AnchorPane semiLayout =null;
 
     private final String navigationItemPath = "/View/FXML/NavigationItem.fxml";
     private final String navigationPath = "/View/FXML/Navigation.fxml";
     private final String barChartPath = "/View/FXML/Barchart.fxml";
     private final String mapPath = "/View/FXML/Map.fxml";
     private final String lineChartPath= "/View/FXML/LineChart.fxml";
+    private final String semiPath = "/View/FXML/SemiContentLayout.fxml";
 
     private NavigationController naviController = null;
     private BarChartController barChartController = null;
     private MapController mapController =null;
+    private LineChartController lineChartController=null;
+    private SemiContentController semiController = null;
+
     enum categorys{
         certificate,region
     }
@@ -48,22 +53,28 @@ public class MainController{
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource(navigationPath));
         this.navigation = (VBox) loader.load();
-        naviController = loader.getController();
+        this.naviController = loader.getController();
 
         loader = new FXMLLoader();
         loader.setLocation(getClass().getResource(barChartPath));
         this.barChartLayout = (Chart) loader.load();
-        barChartController =loader.getController();
+        this.barChartController =loader.getController();
 
         loader = new FXMLLoader();
         loader.setLocation(getClass().getResource(mapPath));
         this.mapLayout = (ImageView) loader.load();
-        mapController = loader.getController();
+        this.mapController = loader.getController();
 
         loader = new FXMLLoader();
-
         loader.setLocation(getClass().getResource(lineChartPath));
         this.lineChartLayout = (Chart) loader.load();
+        this.lineChartController = loader.getController();
+
+        loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(semiPath));
+        this.semiLayout = (AnchorPane) loader.load();
+        this.semiController = loader.getController();
+
     }@FXML
     public void initialize()
     throws IOException {
@@ -71,43 +82,21 @@ public class MainController{
         AnchorPane.setTopAnchor(navigation, 0.0);
         navigationPane.getChildren().add(navigation);
         /*add category*/
-        AnchorPane barChartSemi = getSemi(barChartLayout, "자격증 트렌드");
-        AnchorPane mapSemi = getSemi(mapLayout, "전국 채용 트렌드");
-        AnchorPane lineChartSemi = getSemi(lineChartLayout,"임금률");
+        AnchorPane barChartSemi = semiController.getSemi(barChartLayout, "자격증 트렌드");
+        AnchorPane mapSemi = semiController.getSemi(mapLayout, "전국 채용 트렌드");
+        AnchorPane lineChartSemi = semiController.getSemi(lineChartLayout,"임금률");
                 
         naviController.addNaviItem(wrappingPane,"기술 트렌드",barChartSemi);
         naviController.addNaviItem(wrappingPane,"지역 트렌드",mapSemi);
         naviController.addNaviItem(wrappingPane,"임금 트렌드",lineChartSemi);
         /*set default pane*/
-//        contents.getChildren().add(chartSemi);
+
+        contents.getChildren().add(barChartSemi);
+        mainTitle.setText("기술 트렌드");
+        navigation.getChildren().get(0).getStyleClass().add("selectedItem");
         
     }
-    public AnchorPane getSemi(Region layout, String title) throws IOException {
-        AnchorPane semiContentLayout = (AnchorPane) FXMLLoader.load(getClass().getResource("/View/FXML/SemiContentLayout.fxml"));
-        semiContentLayout.getStyleClass().add("semiPane");
 
-        Label semiTitle = (Label) semiContentLayout.lookup("#semiTitle");
-        semiTitle.setText(title);
-
-        Pane p = (Pane) semiContentLayout.lookup("#contents");
-        layout.prefWidthProperty().bind(p.widthProperty());
-        layout.prefHeightProperty().bind(p.heightProperty());
-
-        p.getChildren().add(layout);
-        return semiContentLayout;
-    }
-    public AnchorPane getSemi(Node layout, String title) throws IOException {
-        AnchorPane semiContentLayout = (AnchorPane) FXMLLoader.load(getClass().getResource("/View/FXML/SemiContentLayout.fxml"));
-        semiContentLayout.getStyleClass().add("semiPane");
-
-        Label semiTitle = (Label) semiContentLayout.lookup("#semiTitle");
-        semiTitle.setText(title);
-        
-        Pane p = (Pane) semiContentLayout.lookup("#contents");
-
-        p.getChildren().add(layout);
-        return semiContentLayout;
-    }
 
 
 //    public void a(){
--- src/Controller/SemiContentController.java
+++ src/Controller/SemiContentController.java
@@ -10,13 +10,12 @@ import javafx.scene.Node;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.scene.web.WebEngine;
@@ -33,5 +32,34 @@ public class SemiContentController {
     @FXML
     public void initialize() throws IOException {
 
+    }
+
+    public AnchorPane getSemi(Region layout, String title) throws IOException {
+        AnchorPane semiContentLayout = (AnchorPane) FXMLLoader.load(getClass().getResource("/View/FXML/SemiContentLayout.fxml"));
+        semiContentLayout.getStyleClass().add("semiPane");
+
+        Label semiTitle = (Label) semiContentLayout.lookup("#semiTitle");
+        Label semiDate =  (Label) semiContentLayout.lookup("#semiDate");
+
+        semiTitle.setText(title);
+        semiDate.setText(LocalDate.now().toString());
+        Pane p = (Pane) semiContentLayout.lookup("#contents");
+        layout.prefWidthProperty().bind(p.widthProperty());
+        layout.prefHeightProperty().bind(p.heightProperty());
+
+        p.getChildren().add(layout);
+        return semiContentLayout;
+    }
+    public AnchorPane getSemi(Node layout, String title) throws IOException {
+        AnchorPane semiContentLayout = (AnchorPane) FXMLLoader.load(getClass().getResource("/View/FXML/SemiContentLayout.fxml"));
+        semiContentLayout.getStyleClass().add("semiPane");
+
+        Label semiTitle = (Label) semiContentLayout.lookup("#semiTitle");
+        semiTitle.setText(title);
+
+        Pane p = (Pane) semiContentLayout.lookup("#contents");
+
+        p.getChildren().add(layout);
+        return semiContentLayout;
     }
 }
